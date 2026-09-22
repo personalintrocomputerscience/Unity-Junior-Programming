69369eb56b068ea513b1cf5bad599c09f23c3b4d
Silas <personalintrocomputerscience@gmail.com>
2026-09-22 08:27:14 -0400

    1.3.1

--- Create With Code/My Project/Assets/Scripts/PlayerController.cs
+++ Create With Code/My Project/Assets/Scripts/PlayerController.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float speed = 5.0f;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +13,7 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        // We'll move the vehicle forward
+        transform.Translate(Vector3.forward * Time.deltaTime * 20);
     }
 }
