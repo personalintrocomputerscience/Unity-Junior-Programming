91009156eb0c32a7a9e26b41813d18e8151b4816
Silas <personalintrocomputerscience@gmail.com>
2026-09-24 07:55:13 -0400

    1.4.5 Debug the input values

--- Create With Code/My Project/Assets/Scripts/PlayerController.cs
+++ Create With Code/My Project/Assets/Scripts/PlayerController.cs
@@ -5,19 +5,25 @@ public class PlayerController : MonoBehaviour
 {
     public float speed = 5.0f;
     public float turnSpeed;
+
     public InputAction moveAction;
+    public Vector2 moveInput;
 
-   // Start is called once before the first execution of Update after the MonoBehaviour is created
-   void Start()
+  // Start is called once before the first execution of Update after the MonoBehaviour is created
+  void Start()
     {
-
+        moveAction.Enable();
     }
 
     // Update is called once per frame
     void Update()
     {
+        moveInput = moveAction.ReadValue<Vector2>();
+
         // We'll move the vehicle forward
         transform.Translate(Vector3.forward * Time.deltaTime * speed);
+
+        // Move the vehicle left and right
         transform.Translate(Vector3.right * Time.deltaTime * turnSpeed);
     }
 }
