1592914c3b391132fb4b3ece116fcf07ea1778c9
Silas <personalintrocomputerscience@gmail.com>
2026-09-24 08:10:04 -0400

    1.4.6 Make the vehicle move forward and sideways

--- Create With Code/My Project/Assets/Scripts/PlayerController.cs
+++ Create With Code/My Project/Assets/Scripts/PlayerController.cs
@@ -21,9 +21,9 @@ public class PlayerController : MonoBehaviour
         moveInput = moveAction.ReadValue<Vector2>();
 
         // We'll move the vehicle forward
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * moveInput.y);
 
         // Move the vehicle left and right
-        transform.Translate(Vector3.right * Time.deltaTime * turnSpeed);
+        transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * moveInput.x);
     }
 }
